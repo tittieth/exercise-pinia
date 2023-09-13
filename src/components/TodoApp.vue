58cd7529050bdab106751e516adafb63818a5731
Tittie <tittie.thomasson@hotmail.se>
2023-09-13 14:25:50 +0200

    add components

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import TodoForm from './TodoForm.vue';
+import TodoList from './TodoList.vue';
+</script>
+
+<template>
+    <div>
+        <h1>To Do List</h1>
+        <TodoForm />
+        <TodoList />
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template><div></div></template>
+
+<style scoped>
+h1 {
+  background-color: aliceblue;
+}
+h2 {
+  background-color: aqua;
+}
+</style>
