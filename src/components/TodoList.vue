e749b698f8c5864836985f7af469f8f08796bd55
Tittie <tittie.thomasson@hotmail.se>
2023-09-14 08:17:44 +0200

    print out todos from the list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useTodoListStore } from '@/stores/todoList';
+import { storeToRefs } from 'pinia';
 
-<template><div></div></template>
+const store = useTodoListStore();
+
+const { todoList } = storeToRefs(store)
+
+</script>
+
+<template>
+<div v-for="todo in todoList" :key="todo.id" class="item">
+    <div class="content">
+        <span>{{ todo.item }}</span>
+    </div>
+
+</div></template>
 
 <style scoped></style>
